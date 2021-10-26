a3e4993bb078d782f1dbd9193e436ff9ba38d477
Andrew Westwick <awestwick@gmail.com>
2021-10-25 22:00:01 -0500

    update players.json [cron]

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running Git Update"
-/mingw64/bin/git add .
-/mingw64/bin/git commit -m "hourly update"
+/mingw64/bin/git add mudscrape-client/content/players.json
+/mingw64/bin/git commit -m "update players.json [cron]"
 /mingw64/bin/git push
 echo "Git update complete"
