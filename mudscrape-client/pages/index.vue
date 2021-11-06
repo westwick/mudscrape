d2e8466960dd595148a0b562ac6b0e7d34211982
Andrew Westwick <awestwick@gmail.com>
2021-11-06 12:45:55 -0500

    show version in footer

--- mudscrape-client/pages/index.vue
+++ mudscrape-client/pages/index.vue
@@ -91,7 +91,7 @@
       </tbody>
     </table>
     <p class="mt-4 mb-4 text-gray-800 text-xs">
-      Whazzis? Whazzat? Hrglflmnblg...
+      Whazzis? Whazzat? Hrglflmnblg... (v0.2.0)
     </p>
   </div>
 </template>
